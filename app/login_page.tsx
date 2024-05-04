bb4deaed808067d31e909098dde146f9a9afd5d2
thanh.ph <phamhoaithanh32@gmail.com>
2024-05-04 20:00:35 +0700

    push code

--- app/login_page.tsx
+++ app/login_page.tsx
@@ -7,34 +7,57 @@ const LoginPage: React.FC = () => {
 
     const [apiResponse, setApiResponse] = useState("null");
 
-    const fetchToken = async () => {
-        const params = new URLSearchParams();
-        params.append('grant_type', 'password');
-        params.append('username', 'user_1_4');
-        params.append('password', 'ALfSzDfky9udTjiBQDil@');
-        params.append('client_id', 'mobile');
-        params.append('client_secret', 'bd5f092087754fed8acc376752b4ce16');
-        params.append('scope', 'openid');
+    // const fetchToken = async () => {
+    //     const params = new URLSearchParams();
+    //     params.append('grant_type', 'password');
+    //     params.append('username', 'user_1_4');
+    //     params.append('password', 'ALfSzDfky9udTjiBQDil@');
+    //     params.append('client_id', 'mobile');
+    //     params.append('client_secret', 'bd5f092087754fed8acc376752b4ce16');
+    //     params.append('scope', 'openid');
+    //
+    //     try {
+    //         const response = await fetch('http://localhost:3000/ids/connect/token', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Accept': 'application/json',
+    //                 'Content-Type': 'application/x-www-form-urlencoded'
+    //             },
+    //             body: params
+    //         });
+    //         const data = await response.json();
+    //         const token = data["access_token"];
+    //         setApiResponse(data);
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // }
 
-        try {
-            const response = await fetch('http://localhost:3000/ids/connect/token', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: params
-            });
+    const fetchLogin = async () => {
+        const response = await fetch('http://localhost:8000/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+            },
+            body: '',
+        });
+
+        console.log('Response:', response);
+        console.log('Response body:', response.body);
+
+        if (response.headers.get('Content-Type') === 'application/json') {
             const data = await response.json();
+            console.log('Data:', data);
             const token = data["access_token"];
             setApiResponse(data);
-        } catch (e) {
-            console.log(e);
+        } else {
+            console.log('Response is not JSON');
         }
     }
 
     useEffect(() => {
-        fetchToken().then(value => {
+        console.log(document.cookie.split(";").map((c) => c.trim() + "\n").toString());
+        fetchLogin().then(value => {
         });
     }, []);
 
@@ -44,12 +67,7 @@ const LoginPage: React.FC = () => {
             {apiResponse && <pre className={"text-black size-2"}>{JSON.stringify(apiResponse, null, 2)}</pre>}
         </div>
         <div className={"h-80 w-80 bg-white mt-10"}>
-            {apiResponse && <pre className={"text-black size-2"}>{JSON.stringify(apiResponse, null, 2)}</pre>}
-        </div>
-        <div className={"flex-col flex  h-10 w-80 bg-blue-950 mt-10 items-center justify-center"}>
-            <a className={""} onClick={async (event) => {
-                await fetchToken()
-            }}> Token</a>
+            <a className={"text-black"}>QR</a>
         </div>
     </div>
 }
